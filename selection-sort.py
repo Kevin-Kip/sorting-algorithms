6f2e590c35123c1ac4923b16e1d002848a1599e0
Kevin-Kip <masterfork5@gmail.com>
2018-08-09 10:34:48 +0300

    Added code for Selection Sort

--- selection-sort.py
+++ selection-sort.py
@@ -1,7 +1,12 @@
 def selection_sort(list_to_sort):
     for i in range(0, len(list_to_sort) - 1):
-        print("Selection")
+        index_of_min_value = i
+        for j in range(i + 1, len(list_to_sort)):
+            if list_to_sort[j] < list_to_sort[index_of_min_value]:
+                index_of_min_value = j
+        if index_of_min_value != i:
+            list_to_sort[i], list_to_sort[index_of_min_value] = list_to_sort[index_of_min_value], list_to_sort[i]
 
 
 if __name__ == "__main__":
-    selection_sort([2])
+    print(selection_sort([9,6,2]))
